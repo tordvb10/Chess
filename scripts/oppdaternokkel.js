6fc0ed8b5952d1a47fbe2b626a7d4f37796da194
tordvb10 <tordvb10.kodehode@gmail.com>
2024-03-01 11:40:36 +0100

    t

--- scripts/oppdaternokkel.js
+++ scripts/oppdaternokkel.js
@@ -1,4 +1,4 @@
-export default function oppdaternokkel(Elements,til, fra) {
+export default function oppdaternokkel(Elements,til, fra) { // Elements = oppdaternokkel(Elements,til, fra) 
     Elements.brett_id[til] = JSON.parse(JSON.stringify(Elements.brett_id[fra]));
     Elements.hvem_sin_tur[til] = JSON.parse(JSON.stringify(Elements.hvem_sin_tur[fra]));
     Elements.riktig_spiller[til] = JSON.parse(JSON.stringify(Elements.riktig_spiller[fra]));
